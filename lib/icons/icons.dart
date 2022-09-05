0b937f83a06edeba54ceec552130e977cc3acaaf
Rowaa <rowaaata67@gmail.com>
2022-09-05 03:02:01 +0200

    animation

---
+++ lib/icons/icons.dart
@@ -0,0 +1,13 @@
+String loadingIcon =
+    'https://assets2.lottiefiles.com/packages/lf20_kbfw3xc4.json';
+String sunnyIcon =
+    'https://assets6.lottiefiles.com/private_files/lf30_rsattbhn.json';
+String partlyCloudyIconNight =
+    'https://assets5.lottiefiles.com/temp/lf20_Jj2Qzq.json';
+String partlyCloudDayIcon =
+    'https://assets5.lottiefiles.com/temp/lf20_Jj2Qzq.json';
+String clearNightIcon = 'https://assets10.lottiefiles.com/packages/lf20_3frputqr.json';
+String rainyIcon =
+    'https://assets7.lottiefiles.com/private_files/lf30_orqfuyox.json';
+String overcastDayIcon =
+    'https://assets10.lottiefiles.com/packages/lf20_1eaisi3u.json';
